412b0696fc1bfd87d7d1bc98a2fdd9ca3ffe3db5
Alberto MOGA <moga.alberto@hotmail.com>
2021-05-20 11:09:10 +0300

    v_1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stripe-payment-v1';
+  paymentHandler: any = null;
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.invokeStripe();
+
+  }
+
+  makePayment(value: number) {
+    const paymentHandler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51IsrYKKvlEtRprkGJnx7tHbrwn0mHX75V8dXkMEhKOJflCNb2XjJLiWnvYzgkOfa7v5ejN20XlKrq8pPyGQhPsR900aSIhSBfG',
+      locale: 'auto',
+      token: function(stripeToken: any) {
+        console.log(stripeToken.card);
+        alert('Stripe token generated!');
+      },
+    });
+
+    paymentHandler.open({
+      name: 'Test name',
+      description: '1 product added',
+      amount: value * 100,
+    })
+  }
+
+  invokeStripe() {
+    if(!window.document.getElementById('stripe-script')) {
+      const script = window.document.createElement('script');
+      script.id = 'stripe-script';
+      script.type = 'text/javascript';
+      script.src = "https://checkout.stripe.com/checkout.js";
+      window.document.body.appendChild(script);
+    }
+  }
 }
+
